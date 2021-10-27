682fa528bb809ea4f71e99777ce10d3c129fb8a1
triettm40 <triettm.40@gmail.com>
2021-10-27 20:23:07 +0630

    Hien thi thong tin shipper

--- app/src/main/java/java/android/quanlybanhang/Shipper/fragment/CaNhanFragment.java
+++ app/src/main/java/java/android/quanlybanhang/Shipper/fragment/CaNhanFragment.java
@@ -2,13 +2,21 @@ package fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.shipper.R;
+import com.example.shipper.Shipper;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +33,8 @@ public class CaNhanFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private DatabaseReference mDatabase;
+    private TextView tv_name,tv_email,tv_phone,tv_date;
 
     public CaNhanFragment() {
         // Required empty public constructor
@@ -61,6 +71,45 @@ public class CaNhanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_canhan, container, false);
+        View view= inflater.inflate(R.layout.fragment_canhan, container, false);
+        tv_name=view.findViewById(R.id.tv_hoten);
+        tv_date=view.findViewById(R.id.tv_ngaysinh);
+        tv_email=view.findViewById(R.id.tv_email);
+        tv_phone=view.findViewById(R.id.tv_sdt);
+        onDataChange();
+        return view;
+    }
+    private void onDataChange()
+    {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("Shipper").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot item : snapshot.getChildren())
+                {
+                    Shipper sp=item.getValue(Shipper.class);
+                    String nameShipper=sp.getNameShipper();
+                    String phoneShipper= sp.getPhoneShipper();
+                    String emailShipper=sp.getEmailShipper();
+                    String dateShipper=sp.getDateShipper();
+                    tv_name.setText(nameShipper);
+                    tv_date.setText(dateShipper);
+                    tv_email.setText(emailShipper);
+                    tv_phone.setText(phoneShipper);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
+    private void changeThongTin(){
+
     }
 }
